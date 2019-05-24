a56751d1ef3a790051d38dc4921c78f0fb2b33ee
quannguyennn <wantmorexx9x@gmail.com>
2019-05-24 11:28:36 +0700

    .

--- src/components/AdsBanner.js
+++ src/components/AdsBanner.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-
+import {Link} from "react-router-dom";
 class AdsBanner extends Component {
     render() {
         return (
-            <a href="/signup" class="banner banner--gradient-bg">
+            <div class="banner banner--gradient-bg">
                 <div class="banner__logo-wrapper">
                     <i class="fi fi-codepen"></i>
                 </div>
                 <div>
                     <div class="banner__title">HOT SALE</div>
-                    <div class="banner__text">Become a member of ShopCANA to receive sale code and huge prices for the first 200!</div>
+                    <div class="banner__text">Become a member of ShopCANA to receive promotion code and huge prices for the first 200!</div>
                 </div>
-                <div class="banner__cta">
-                    SIGN UP
-                </div>
-            </a>
+                <Link class="banner__cta" to="/signup">
+                    <div >
+                        SIGN UP
+                    </div>
+                </Link>
+                
+            </div>
 
         );
     }
