b1e659f022b16a96f858e5f0ea54e14921c32abc
quannguyennn <wantmorexx9x@gmail.com>
2019-05-29 21:20:53 +0700

    final ui

--- src/views/ProductDetail.js
+++ src/views/ProductDetail.js
@@ -9,6 +9,7 @@ import {Button} from "antd";
 import { UserContext } from '../contexts/User';
 import { Modal } from 'antd';
 
+
 function warning() {
     Modal.warning({
       title: 'Warning',
@@ -22,37 +23,52 @@ class ProductDetail extends Component {
         super(props);
         this.state = {
             product: {},
+            products: []
         }
     }
-    componentDidMount() {
+    componentWillMount() {
         window.scrollTo(0,0);
         axios.get( `https://xcommerce-server.herokuapp.com/api/products/${this.props.match.params.productid}`)
             .then(data => {
+                console.log(data.data.data)
                 this.setState({
-                    product: data.data.data
+                    product: data.data.data,
                 })
+                axios.get("https://xcommerce-server.herokuapp.com/api/products/category/" + data.data.data.category)
+                .then(data => {
+                    this.setState({
+                        products: data.data.data
+                    })
+                })
+                .catch(err => console.log(err))
             })
             .catch(err => console.error(err))
     }
 
     render() {
-        const { product } = this.state;
+        const { product, products } = this.state;
         const discription = product.discription ? product.discription : "";
-        console.log(discription);
         let discriptionText = discription.split("/");
+        const recommend = !_.isEmpty(products) ? products.map(product => (
+            <Grid item xs={2} key={product._id} style={{ margin: "10px"}}>
+                <a href={`/products/${product._id}`}  style={{textDecoration: "none", textDecorationColor: "none"}}>
+                    <img className="small-img" src={product.imageUrl} alt="" style={{ padding: "4px",width: "120px", height: "150px"}}/>
+                </a>
+            </Grid>
+        )) : "As"
         return (
             <Grid container spacing={16} >
                 <Grid item xs={12} >
                     <NavbarSmall />
                 </Grid>
-                <Grid item xs={12} style={{margin: "120px auto 0", maxWidth: "70vw"}}>
+                <Grid item xs={12} style={{margin: "120px auto 0", maxWidth: "70vw", borderBottom: "2px solid black"}}>
                     <Grid container style={{ display: "flex", justifyContent: "center"}}>
                         <Grid item xs={6} style={{width: "70%", height: "100%"}}>
                             <img src={product.imageUrl} alt="" style={{width: "55%", height: "100%"}}/>
                         </Grid>
                         <Grid item xs={6}>
                             <h1>{product.title}</h1>
-                            {discriptionText.map(text => <h5>{text}</h5>)}
+                            {discriptionText.map((text, index) => <h5 key={index}>{text}</h5>)}
                             <h5>${product.price}</h5>
                             <CartContext.Consumer>
                                 {
@@ -74,6 +90,14 @@ class ProductDetail extends Component {
                     </Grid>
                     
                 </Grid>
+
+                <Grid item xs={12} style={{margin: "0 auto", maxWidth: "70vw"}}>
+                    <h5>Product you may want to see</h5>
+                    <Grid container>
+                        {recommend}
+                    </Grid>
+                </Grid>
+
                 <Grid item xs={12} style={{marginTop: "43px"}}>
                     <Footers />
                 </Grid>
