fdef13809535479d3c178c3733b41e089801f5ff
quannguyennn <wantmorexx9x@gmail.com>
2019-06-02 04:59:50 +0700

    .

--- src/views/ProductDetail.js
+++ src/views/ProductDetail.js
@@ -4,12 +4,12 @@ import Footers from "../components/Footer"
 import { Grid} from "@material-ui/core";
 import axios from "axios";
 import {CartContext} from "../contexts/Cart";
+import { Link } from "react-router-dom";
 import _ from "lodash";
 import {Button} from "antd";
 import { UserContext } from '../contexts/User';
 import { Modal } from 'antd';
 import { css } from '@emotion/core';
-
 import { PulseLoader } from 'react-spinners';
 
 const override = css`
@@ -32,16 +32,18 @@ class ProductDetail extends Component {
         super(props);
         this.state = {
             product: {},
-            products: []
+            products: [],
+            id: ""
         }
     }
     componentWillMount() {
         window.scrollTo(0,0);
         axios.get( `https://xcommerce-server.herokuapp.com/api/products/${this.props.match.params.productid}`)
             .then(data => {
-                console.log(data.data.data)
+                console.log(data.data.data._id)
                 this.setState({
                     product: data.data.data,
+                    id: data.data.data._id
                 })
                 axios.get("https://xcommerce-server.herokuapp.com/api/products/category/" + data.data.data.category)
                 .then(data => {
@@ -54,15 +56,30 @@ class ProductDetail extends Component {
             .catch(err => console.error(err))
     }
 
+    componentDidUpdate(prevProps, prevStates){
+        if(prevStates.id !== this.state.id){
+            axios.get(`https://xcommerce-server.herokuapp.com/api/products/${this.state.id}`)
+            .then(data => this.setState({ product: data.data.data,}))
+            .catch(err => console.log(err))
+        }
+    }
+
+    changeProduct = () => {
+        console.log(window.location.href)
+        let ids = window.location.href.split("/");
+        let id = ids[ids.length - 1];
+        this.setState({id: id})
+    }
+
     render() {
         const { product, products } = this.state;
         const discription = product.discription ? product.discription : "";
         let discriptionText = discription.split("/");
         const recommend = !_.isEmpty(products) ? products.map(product => (
-            <Grid item xs={2} key={product._id} style={{ margin: "10px"}}>
-                <a href={`/products/${product._id}`}  style={{textDecoration: "none", textDecorationColor: "none"}}>
+            <Grid item xs={2} key={product._id} style={{ margin: "10px"}} onClick={this.changeProduct}>
+                <Link to={`/products/${product._id}`}  style={{textDecoration: "none", textDecorationColor: "none"}}>
                     <img className="small-img" src={product.imageUrl} alt="" style={{ padding: "4px",width: "120px", height: "150px"}}/>
-                </a>
+                </Link>
             </Grid>
         )) : (<div className='sweet-loading'>
                 <PulseLoader
